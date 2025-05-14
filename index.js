f5b72880a89772c5b4cc3cf06c92bc5ba7b05f54
nusratmomita <108608844+nusratmomita@users.noreply.github.com>
2025-05-15 01:40:13 +0600

    Created new coffee servr added

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const port  = process.env.PORT || 3000;
 
+
 app.use(cors());// to have access from other port 
 app.use(express.json());// to able to use json data
+
+
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.1k8uoge.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    
+    // ! doing CRUD's C -> create
+    // to add coffee(AddCoffee.jsx) 
+    const coffeeCollection = client.db("coffeeStoreDB").collection("addNewCoffee")
+    app.post('/addNewCoffee',async(req,res)=>{
+        const newCoffee = req.body;
+        // console.log(newCoffee)
+        const result = await coffeeCollection.insertOne(newCoffee)
+        res.send(result);
+    })
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 // root 
 app.get('/',(req,res)=>{
